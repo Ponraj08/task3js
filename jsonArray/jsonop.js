af7a49a3d1f86b58269d4db320d0c221f3942ae2
Ponraj08 <ponrajmuruganantham@gmail.com>
2025-03-13 23:45:09 +0530

    commiting new changes

--- jsonArray/jsonop.js
+++ jsonArray/jsonop.js
@@ -1,3 +1,94 @@
-let promise=new Promise fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
+
+async function fetching() {
+
+      const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'GET',
+
+      })
+
+
+
+      return response.json();
+
+
+}
+
+fetching().then((result) => {
+
+
+      // console.log(result)
+
+
+      const ans = document.getElementById("ans")
+      const table=document.getElementById("table")
+
+
+
+      let p = ''
+
+      // for (let i = 0; i < result.length; i++) {
+
+
+      //       for (let key in result[i]) {
+
+                  
+
+      //             p += `${key} : ${result[i][key]} <br />`
+
+      //       }
+
+
+      // }
+
+
+      
+      
+      const colm=document.createElement("tr")
+
+
+      
+
+      for(let i=0;i<result.length;i++){
+            
+            
+            console.table(result[i])
+            
+            
+            x=Object.keys(result[i])
+            
+            
+            
+            for (var j=0;j<x.length;j++){
+                  
+                  key=x[j]
+                  
+                  value=result[i][key]
+                  
+                  // console.log(key,value);
+
+                  p += `${key} : ${value} <br />`
+
+              }
+
+
+            // for(let j=0;j<result.length;j++){
+
+            //       console.log(j)
+
+            // }
+            
+            // console.log(Object.keys(result[i]))
+
+
+            // console.log(Object.values(result[i]))
+           
+
+            
+
+
+            
+      }
+
+
+       ans.innerHTML = p
+})
